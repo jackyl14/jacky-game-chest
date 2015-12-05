076d3de8f2f5bcd7d3cb5722f8f3b18c23b74d85
Jacky Lee <jackylee@Jacky-Lees-MacBook-Pro.local>
2015-12-05 14:33:37 -0800

    Used rails new to create a new game app
    The purpose of this app is to develop simple
    games for people to play. Installing the haml
    gem was the only modification made to the rails
    default.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JackyGameChest::Application.config.secret_key_base = '277105b6831b1256680b8dc7a0737e0ebb4aabb68908c2601d534bd2547947ffb17724c44548e2df9f295c1591b14ac1fe7a84b82036cdee9d67cc73bb0cfa7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JackyGameChest::Application.config.session_store :cookie_store, key: '_jacky-game-chest_session'
